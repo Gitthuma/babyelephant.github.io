99b253a728608f0872b439262dfc16240d185019
George Githuma <george.githuma@gmail.com>
2019-02-19 01:52:34 +0300

    Added space between BabyElephant and myName/storedName

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
     var myName = prompt('Please enter your name');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'BabyElephant' + myName;
+    myHeading.textContent = 'BabyElephant ' + myName;
 }
 
 if (!localStorage.getItem('name')) {
     setUserName();
 } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'BabyElephant' + storedName;
+    myHeading.textContent = 'BabyElephant ' + storedName;
 }
 
 myButton.onclick = function() {
