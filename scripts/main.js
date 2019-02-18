e157d8ef104de68d09565fb036dd294296762200
George Githuma <george.githuma@gmail.com>
2019-02-18 23:45:29 +0300

    Added code to main.js that changes the image on index.html onnclick from elephant rolling on ball to elephant eating popcorn

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
+var myImage = document.querySelector('img');
 
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/animated-elephant-image-0456.gif') {
+        myImage.setAttribute('src', 'images/indian-elephant-popcorn-animation-baby-elephant-eating-popcorn-thumb.jpg')
+    } else {
+        myImage.setAttribute('src', 'images/animated-elephant-image-0456.gif')
+    }
+}
